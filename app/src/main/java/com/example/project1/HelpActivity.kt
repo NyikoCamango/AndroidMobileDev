a458cfbf0dde55d87a27901f22b0d85618157220
NyikoCamango <217026744@student.uj.ac.za>
2023-05-16 13:13:31 +0200

    added more functionalities

--- app/src/main/java/com/example/project1/HelpActivity.kt
+++ app/src/main/java/com/example/project1/HelpActivity.kt
@@ -5,12 +5,21 @@ import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.Button
 import android.widget.ImageView
+import android.widget.TextView
 
 class HelpActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_help)
+
+        lateinit var welcomeText: String
+        lateinit var tvWelcome: TextView
+
+        welcomeText ="Welcome "+ getIntent().getStringExtra("Username").toString() + "!";
+        tvWelcome = this.findViewById(R.id.tvWelcome);
+        tvWelcome.setText(welcomeText);
+
 
         val etButtonHelp: Button = findViewById(R.id.btnHelp)
 
